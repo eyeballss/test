68a604b6c04d595377bd17e3cd94c2266fade8df
eyeballs <kkvv1111@gmail.com>
2016-06-14 11:06:11 +0900

    16.6.14 add more testes in StaticManagerTest

--- app/src/androidTest/java/helper/StaticManagerTest.java
+++ app/src/androidTest/java/helper/StaticManagerTest.java
@@ -22,6 +22,9 @@ public class StaticManagerTest extends TestCase {
     @Test
     public void testSortByValue() throws Exception {
         HashMap<String, Double> map = new HashMap<String, Double>();
+        List list;
+
+        //Test1
         map.put("a", 0.0);
         map.put("b", -0.1);
         map.put("c", 0.1);
@@ -29,7 +32,7 @@ public class StaticManagerTest extends TestCase {
         map.put("e", 0.9);
         map.put("f", 1.1);
 
-        List list = StaticManager.sortByValue(map);
+        list = StaticManager.sortByValue(map);
 
         assertEquals("b", list.get(0));
         assertEquals("a", list.get(1));
@@ -38,5 +41,77 @@ public class StaticManagerTest extends TestCase {
         assertEquals("d", list.get(4));
         assertEquals("f", list.get(5));
 
+        map.clear();
+        list.clear();
+
+        //Test2
+        map.put("a",-9.9);
+        map.put("b",-8.8);
+        map.put("c",-7.7);
+        map.put("d",-6.6);
+        map.put("e",-5.5);
+
+        list = StaticManager.sortByValue(map);
+
+        assertEquals("e", list.get(0));
+        assertEquals("d", list.get(1));
+        assertEquals("c", list.get(2));
+        assertEquals("b", list.get(3));
+        assertEquals("a", list.get(4));
+
+
+        map.clear();
+        list.clear();
+
+        //Test3
+        map.put("a",0.00001);
+        map.put("b",0.00002);
+        map.put("c",0.00003);
+        map.put("d",-0.0001);
+        map.put("e",-0.0002);
+
+        list = StaticManager.sortByValue(map);
+
+        assertEquals("e", list.get(0));
+        assertEquals("d", list.get(1));
+        assertEquals("c", list.get(2));
+        assertEquals("b", list.get(3));
+        assertEquals("a", list.get(4));
+
+        map.clear();
+        list.clear();
+
+        //Test4
+        map.put("a",0.0);
+        map.put("b",-10.0);
+        map.put("c",100.0);
+        map.put("d",1000.0);
+        map.put("e",-100.0);
+
+        list = StaticManager.sortByValue(map);
+
+        assertEquals("e", list.get(0));
+        assertEquals("b", list.get(1));
+        assertEquals("a", list.get(2));
+        assertEquals("c", list.get(3));
+        assertEquals("d", list.get(4));
+
+        map.clear();
+        list.clear();
+
+        //Test5
+        map.put("a", 5.55555);
+        map.put("b", 4.44444);
+        map.put("c", 3.33333);
+        map.put("d", 2.22222);
+        map.put("e", 1.11111);
+
+        list = StaticManager.sortByValue(map);
+
+        assertEquals("e", list.get(0));
+        assertEquals("d", list.get(1));
+        assertEquals("c", list.get(2));
+        assertEquals("b", list.get(3));
+        assertEquals("a", list.get(4));
     }
 }
